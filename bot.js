c0b980451db87917388cb70a0ca4d7d92f7bf73b
Medyumcuk <75220091+Medyumcuk@users.noreply.github.com>
2021-01-15 23:11:22 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,208 @@
+const Discord = require("discord.js");
+require("events").EventEmitter.defaultMaxListeners = 30000;
+  require("events").defaultMaxListeners = 30000;
+const client = new Discord.Client();
+const ayarlar = require("./ayarlar.json");
+const kanal = ayarlar.kanal;
+const fs = require("fs");
+require("./util/eventLoader")(client);
+const express = require("express");
+const app = express();
+const http = require("http");
+var Jimp = require('jimp');
+app.get("/", (request, response) => {
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+const log = message => {
+  console.log(` => { ${message} } `);
+  
+};
+
+client.commands = new Discord.Collection();
+client.aliases = new Discord.Collection();
+fs.readdir("./komutlar/", (err, files) => {
+  if (err) console.error(err);
+  log(`${files.length} komut yüklenecek.`);
+  files.forEach(f => {
+    let props = require(`./komutlar/${f}`);
+    log(`AKTİF: ${props.help.name}.`);
+    client.commands.set(props.help.name, props);
+    props.conf.aliases.forEach(alias => {
+      client.aliases.set(alias, props.help.name);
+    });
+  });
+});
+
+client.reload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.load = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.set(command, cmd);
+      cmd.conf.aliases.forEach(alias => {
+        client.aliases.set(alias, cmd.help.name);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+client.unload = command => {
+  return new Promise((resolve, reject) => {
+    try {
+      delete require.cache[require.resolve(`./komutlar/${command}`)];
+      let cmd = require(`./komutlar/${command}`);
+      client.commands.delete(command);
+      client.aliases.forEach((cmd, alias) => {
+        if (cmd === command) client.aliases.delete(alias);
+      });
+      resolve();
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+////////////////////////
+
+client.elevation = message => {
+  if (!message.guild) {
+    return;
+  }
+  let permlvl = 0;
+  if (message.member.hasPermission("ADMINISTRATOR")) permlvl = 3;
+  return permlvl;
+};
+
+client.login(ayarlar.token);
+
+
+
+
+client.on("ready", () => {//splashen
+  client.user.setPresence({
+    game: { name: ` Batu <3 Ayris `, type: "WATCHING" },
+    status: "online"
+  });
+});
+
+
+
+// İSİM YAŞ İSİM DEĞİŞTİRME 
+
+client.on("guildMemberAdd", member => {
+  let tag = ayarlar.tag;
+  //splashen
+  member.setNickname(`${tag} İsim | Yaş`);
+});
+
+// İSİM YAŞ İSİM DEĞİŞTİRME SON
+
+
+
+
+
+
+//BOT ROLÜ
+
+client.on(`guildMemberAdd`, async member => {//splashen
+  let botrol = ayarlar.botROL;
+if(!member.bot) return;
+member.roles.add(botrol)
+})
+
+// BOT ROLÜ SON
+
+
+
+
+// kayıtsız rolü
+
+client.on(`guildMemberAdd`, async member => {
+  let kayıtsızROL = ayarlar.kayıtsızROL;
+if(member.bot) return;
+member.roles.add(kayıtsızROL)
+})
+
+/// kayıtsız rolü son
+//splashen
+
+
+
+//splashen
+
+// BOT OTOROL
+
+client.on('guildMemberAdd', async member => {//splashen
+if(member.user.bot) {
+  const botROL = ayarlar.botROL
+member.roles.add(botROL)
+}
+})
+// GİRİŞ 
+  client.on("guildMemberAdd", member => { 
+    const moment = require('moment');
+  const kanal = ayarlar.giriskanal;
+  let user = client.users.cache.get(member.id);
+  require("moment-duration-format");
+    const tarih = new Date().getTime() - user.createdAt.getTime();  
+  const embed = new Discord.MessageEmbed()
+  let rol = ayarlar.kayıtsızROL
+ member.roles.add(rol)//splashen
+
+  var kontrol;
+if (tarih < 1296000000) kontrol = '<a:vnt_emoji_2:784852585156050967> Bu Kullanıcı **Şüpheli**'
+if (tarih > 1296000000) kontrol = '<a:vnt_emoji_2:784852512833929216> Bu Kullanıcı **Güvenli**'
+  moment.locale("tr");
+  let kanal1 = client.channels.cache.find(x => x.id === kanal);
+    let giris = new Discord.MessageEmbed()
+    .setDescription(`
+●▬▬▬▬▬▬▬▬ <a:vnt_emoji_2:784853553419517982> **Bir Üye Katıldı** <a:vnt_emoji_2:784853553419517982> ▬▬▬▬▬▬▬▬●
+
+• <a:vnt_emoji_2:784852682946641950> Hoşgeldin ${member}
+
+• <a:vnt_emoji_2:784854051417227284> Seninle birlikte **${member.guild.memberCount}** kişiyiz.
+
+• [ **${ayarlar.tag}** ] Tagımızı alarak ekibimize katılabilirsin.
+
+• <a:vnt_emoji_20:784856423686340608> <@&${ayarlar.yetkiliROL}> rolündekiler seninle ilgilenicektir.
+
+• <<a:vnt_emoji_2:784862301048143933> Hesabın Oluşturulma Tarihi: \n • \` ${moment(member.user.createdAt).format("YYYY DD MMMM dddd (hh:mm:ss)")} \`
+
+•  ${kontrol} 
+
+• <a:vnt_emoji_2:784854720119570493> Ses teyit odasında kaydınızı yaptırabilirsiniz. 
+
+`)//splashen
+    .setImage('https://i.pinimg.com/originals/2c/43/ac/2c43acd8c41ee853cf9fbb04960e4fa6.gif')
+    .setTimestamp()
+client.channels.cache.find(x => x.id === kanal).send(giris)
+  });
+// GİRİŞ SON
+//splashen
